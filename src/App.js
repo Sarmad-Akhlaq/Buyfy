f642a2f38d5d1656329672564beb14aabbc9ff6c
Sarmad-Akhlaq <sarmadakhlaq.66@gmail.com>
2021-03-19 18:46:38 +0500

    done with initial web skeleton

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
+import {Switch, Route } from 'react-router-dom';
+import Home from './Pages/Home/Home';
+import Authentication from './Pages/Authentication/Authentication';
+import Category from './Pages/Category/Category';
+import CategoryProducts from './Pages/CategoryProducts/Categoryproducts';
+import Checkout from './Pages/Checkout/Checkout';
 
 function App() {
   return (
-    <div className="App">
-        <h1>Buyfy</h1>
-    </div>
-  );
+    <Switch>
+      <Route path="/" component={Home} exact />
+      <Route path="/authentication" component={Authentication} exact />
+      <Route path="/categories" component={Category} exact />
+      <Route path="/category-products" component={CategoryProducts} exact />
+      <Route path="/checkout" component={Checkout} exact />
+    </Switch>
+    );
 }
 
 export default App;
